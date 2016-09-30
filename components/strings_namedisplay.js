8f87de0cb3f21d04e270d2f4371787fcdd966a55
Andrew Mouat <plz_nosparem_thnx@hotmail.com>
2016-10-01 02:16:43 +1000

    readjusted moet line

--- components/strings_namedisplay.js
+++ components/strings_namedisplay.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 var NameDisplay_Strings = 
 	{'en': {name: 'Andrew Mouat', 
-			furigana: '                                        pronounced ' +
+			furigana: '                                                      ' +
+				'pronounced ' +
 				'<a href="http://www.stuff.co.nz/life-style/food-wine/drinks/67683952/french-winemaker-clears-up-moet-issue" ' +
 				'target="_blank">Moët</a>'},
 		'jp': {name: '暗怒龍', furigana: 'アンドリュー'}};
