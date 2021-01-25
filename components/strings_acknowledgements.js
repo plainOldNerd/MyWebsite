b97b70a1979dc71b7ac428e3ad686c5448ccecaf
Xavier Mouat <XavierMouat@gmail.com>
2021-01-26 10:41:36 +1100

    Update strings_acknowledgements.js

--- components/strings_acknowledgements.js
+++ components/strings_acknowledgements.js
@@ -1,6 +1,5 @@
 exports.strings = {'en': {heading: 'Acknowledgements', 
-	description: '<ul> <li> <a href="http://andrewhfarmer.com/build-your-own-starter/#0-intro" ' +
-		'target="_blank">Build Your Own Starter</a> helped hugely in ' +
+	description: '<ul> <li> http://andrewhfarmer.com/build-your-own-starter/#0-intro (link now outdated) helped hugely in ' +
 		'understanding the technological dependancies involved. I owe an immense ' +
 		'thanks to this site. </li>' +
 		'<li> Alnitak\'s explanation <a href="http://stackoverflow.com/questions/5311334/what-is-the-purpose-of-node-js-module-exports-and-how-do-you-use-it/5311377#5311377" ' +
@@ -8,8 +7,7 @@ exports.strings = {'en': {heading: 'Acknowledgements',
 		'<li> There were also some answers on StackOverflow that helped in solving ' +
 		'some small problems. </li> </ul>'}, 
 	'jp': {heading: '助かった', 
-		description: '<ul> <li> <a href="http://andrewhfarmer.com/build-your-own-starter/#0-intro" ' +
-		'target="_blank">Build Your Own Starter</a>はウェッブページが作れる前に必要なこと' +
+		description: '<ul> <li> http://andrewhfarmer.com/build-your-own-starter/#0-intro(リンクが古くなった)はウェッブページが作れる前に必要なこと' +
 		'を理解することに大分助けてくれた。信じられんほど感謝してる。 </li>' +
 		'<li> <a href="http://stackoverflow.com/questions/5311334/what-is-the-purpose-of-node-js-module-exports-and-how-do-you-use-it/5311377#5311377" ' +
 		'target="_blank">ここ</a>でのAlnitakのrequire()の説明は理解する為にパーフェクトだった。 </li>' +
