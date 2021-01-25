10f869d7014d1948dc3180eedfa222901be4939b
Xavier Mouat <XavierMouat@gmail.com>
2021-01-26 10:43:58 +1100

    Update strings_namedisplay.js

--- components/strings_namedisplay.js
+++ components/strings_namedisplay.js
@@ -1,8 +1,8 @@
 var NameDisplay_Strings = 
-	{'en': {name: 'Andrew Mouat', 
-			furigana: '                                         pronounced ' +
+	{'en': {name: 'Xavier Mouat', 
+			furigana: '                                  pronounced ' +
 				'<a href="http://www.stuff.co.nz/life-style/food-wine/drinks/67683952/french-winemaker-clears-up-moet-issue" ' +
 				'target="_blank">Moët</a>'},
-		'jp': {name: '暗怒龍', furigana: 'アンドリュー'}};
+		'jp': {name: 'ザビア', furigana: '　　　　　　'}};
 
 export {NameDisplay_Strings};
